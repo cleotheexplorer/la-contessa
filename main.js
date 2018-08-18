994b946d1a6059e35406885ed74792219033149e
Eddrich Janzzen Ang <eddrich.ang@obf.ateneo.edu>
2018-08-18 15:48:53 +0800

    Fix assets, addimage slideshow capabilities

--- main.js
+++ main.js
@@ -18,10 +18,44 @@ $('a[href*="#"]')
         // Only prevent default if animation is actually gonna happen
         event.preventDefault();
         $('html, body').animate({
-          scrollTop: target.offset().top -90
-        }, 1200, function() {
+          scrollTop: target.offset().top -70
+        }, 1000, function() {
           // Callback after animation
         });
       }
     }
   });
+
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1} 
+  if (n < 1) {slideIndex = slides.length}
+  
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none"; 
+  }
+  for (i = 0; i < dots.length; i++) {
+      // console.log(dots[i].className)
+      dots[i].className = dots[i].className.replace(" active", "");
+      
+  }   
+
+  slides[slideIndex-1].style.display = "block"; 
+  dots[slideIndex-1].className += " active";
+}
